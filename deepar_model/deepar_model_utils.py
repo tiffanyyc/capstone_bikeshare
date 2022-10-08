5e9e3ce75764d6b88bcd3119076dfc6e3b8841dc
Tiffany Cheng <tiffanyycheng@berkeley.edu>
2022-10-08 18:18:52 +0000

    tidied up code and created charts

--- deepar_model/deepar_model_utils.py
+++ deepar_model/deepar_model_utils.py
@@ -2,30 +2,49 @@ import pandas as pd
 from tqdm import tqdm
 
 
+# clean and prepare trips data
+def prep_station_data(data, station_col, station_time):
+    
+    # clean trips data
+    trips_clean = data[[station_col, station_time]]
+    trips_clean[station_time] = pd.to_datetime(trips_clean[station_time])
+    
+    # prepare station data
+    trips_clean_group = trips_clean.groupby([station_time, station_col], as_index = False).size()
+    
+    return trips_clean_group
+
+# get individual station id and standardize
+def get_station_data(data, station_col, station_time, station, freq, max_date):
+    
+    # filter for each station
+    temp_series = data[data[station_col] == station]
+    temp_series.set_index(station_time, inplace = True)
+    
+    # downsample trips to freq increments
+    temp_series_freq = temp_series.resample(freq).sum()
+    temp_daterange = pd.date_range(start = temp_series_freq.index[0], end = max_date, freq = freq)
+    
+    # ensure the series reaches the end/max_date
+    temp_series_freq = temp_series_freq.reindex(temp_daterange).fillna(0)
+    temp_series_freq = temp_series_freq.drop([station_col], axis = 1)
+    temp_series_freq["size"] = temp_series_freq["size"].astype(int)
+    
+    return temp_series_freq
+
 # organize data required for the DeepAR model
-def organize_data(data, station_col, max_date):
+def organize_station_data(data, station_col, station_time, freq, max_date):
     
     station_dict = {}
     
     for station in tqdm(data[station_col].unique()):
 
         temp_dict = {}
-
-        # filter for each station
-        temp_series = data[data[station_col] == station]
-        temp_series.set_index("starttime", inplace = True)
-
-        # downsample trips to 15-minute increments
-        temp_series_15min = temp_series.resample("15min").sum()
-        temp_daterange = pd.date_range(start = temp_series_15min.index[0], end = max_date, freq = "15min")
-
-        # ensure the series reaches the end/max_date
-        temp_series_15min = temp_series_15min.reindex(temp_daterange).fillna(0)
-        temp_series_15min["size"] = temp_series_15min["size"].astype(int)
+        temp_series_freq = get_station_data(data, station_col, station_time, station, freq, max_date)
 
         # specify the start of the series and the series itself
-        temp_dict = {"start": str(temp_series_15min.index[0]),
-                     "target": temp_series_15min["size"].tolist()}
+        temp_dict = {"start": str(temp_series_freq.index[0]),
+                     "target": temp_series_freq["size"].tolist()}
 
         station_dict[station] = temp_dict
         
